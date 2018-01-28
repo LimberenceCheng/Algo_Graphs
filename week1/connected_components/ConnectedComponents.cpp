2eaec9518755a03f5e5a9023970f608724475f17
程泽华 <chengzehua@outlook.com>
2018-01-28 11:24:21 +0800

    ConnectedComponents in cpp

---
+++ week1/connected_components/ConnectedComponents.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using std::vector;
+using std::pair;
+
+
+void reach(vector<pair<bool,vector<int> > > &adj, int x)
+{
+  //DFS implementation
+  adj[x].first = 1;
+  for(int i=0; i<(int)adj[x].second.size(); i++)
+  {
+	  if(adj[(adj[x].second)[i]].first == 0)
+	  {
+		  reach(adj,((adj[x].second)[i]));
+	  }
+  }
+}
+
+int main() {
+  size_t n, m;
+  int counter = 0;
+  std::cin >> n >> m;
+  vector<pair<bool,vector<int> > > adj(n);
+
+  for(int i=0; i<(int)n; i++)
+	  adj[i].first = 0;
+
+  for (size_t i = 0; i < m; i++) {
+    int x, y;
+    std::cin >> x >> y;
+    adj[x - 1].second.push_back(y - 1);
+    adj[y - 1].second.push_back(x - 1);
+  }
+
+  for(int i=0; i<(int)n; i++)
+  {
+	  if(adj[i].first == 0)
+	  {
+		  reach(adj,i);
+		  counter++;
+	  }
+  }
+  std::cout << counter << std::endl;
+  return 0;
+}
